5a16ab2cc3b829c9140459e72118ef8537da7173
Jaye Gao <810915983@qq.com>
2017-07-17 23:57:39 +0800

    添加侧滑界面

--- src/page/MindCenter/selfCenter.js
+++ src/page/MindCenter/selfCenter.js
@@ -20,11 +20,16 @@ export default class SelfCenter extends Component {
     render() {
         return (
             <View style={Styles.container}>
-                <Image style={{justifyContent:'flex-start',width:'100%',height:190}} resizeMode={Image.resizeMode.stretch} source={require("../../../res/images/navigatop.jpg")}></Image>
-                <TouchableHighlight  underlayColor='#f2f2f2' style={{marginTop:5,backgroundColor:'#d6d6d6'}} onPress={()=>{ }}  >
+                <Image style={{justifyContent:'center',width:'100%',height:190}} resizeMode={Image.resizeMode.stretch} source={require("../../../res/images/navigatop.jpg")}>
+                    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                        <Image style={{height:60,width:60}} resizeMode={Image.resizeMode.stretch} source={require('../../../res/images/header.png')}></Image>
+                        <Text style={{textAlignVertical:'center',marginTop:5,fontSize:18}}>Jaye_Gao</Text>
+                    </View>
+                </Image>
+                <TouchableHighlight  underlayColor='#f2f2f2' style={{marginTop:1,backgroundColor:'#d6d6d6'}} onPress={()=>{ }}  >
                     <View style={{width:'100%',height:40,flexDirection:'row',justifyContent:'space-between'}}>
                         <View style={{height:40,flexDirection:'row',alignItems:'center'}}>
-                            <Image style={{marginLeft:15,width:22,height:22}} source={require('../../../res/images/iconmyyuyue.png')}></Image>
+                            <Image style={{marginLeft:15,width:20,height:20}} source={require('../../../res/images/iconmyyuyue.png')}></Image>
                             <Text style={{marginLeft:15,fontSize:16,color:'#333'}}>我的预约</Text>
                         </View>
                         <View style={{height:40,flexDirection:'row',alignItems:'center'}}>
@@ -41,12 +46,14 @@ export default class SelfCenter extends Component {
                             <Text style={{marginTop:8,fontSize:14}}>待确认</Text>
                         </View>
                     </TouchableHighlight>
+                    <View style={{backgroundColor:'#cdcdcd',height:'100%',width:1}}></View>
                     <TouchableHighlight  underlayColor='#f2f2f2' style={{height:80,width:'33%',justifyContent:'center',backgroundColor:'#d6d6d6'}} onPress={()=>{ }}  >
                         <View style={{alignItems:'center'}}>
                             <Image style={{width:25,height:25}} source={require('../../../res/images/icon_car.png')}></Image>
                             <Text style={{marginTop:8,fontSize:14}}>待练车</Text>
                         </View>
                     </TouchableHighlight>
+                    <View style={{backgroundColor:'#cdcdcd',height:'100%',width:1}}></View>
                     <TouchableHighlight  underlayColor='#f2f2f2' style={{height:80,width:'33%',justifyContent:'center',backgroundColor:'#d6d6d6'}} onPress={()=>{ }}  >
                         <View style={{alignItems:'center'}}>
                             <Image style={{width:25,height:25}} source={require('../../../res/images/icon_ok.png')}></Image>
@@ -54,18 +61,35 @@ export default class SelfCenter extends Component {
                         </View>
                     </TouchableHighlight>
                 </View>
-                <TouchableHighlight  underlayColor='#f2f2f2' style={{marginTop:5,backgroundColor:'#d6d6d6'}} onPress={()=>{ }} >
+                <View style={{backgroundColor:'#cdcdcd',height:5,width:'100%'}}></View>
+                <TouchableHighlight  underlayColor='#f2f2f2' style={{backgroundColor:'#d6d6d6'}} onPress={()=>{ }} >
                     <View style={{width:'100%',height:50,flexDirection:'row',alignItems:'center'}}>
                         <Image style={{marginLeft:15,width:22,height:22}} source={require('../../../res/images/iconjiaolian.png')}></Image>
                         <Text style={{marginLeft:15,fontSize:16,color:'#333'}}>我的教练</Text>
                     </View>
                 </TouchableHighlight>
-                <TouchableHighlight  underlayColor='#d6d6d6' onPress={()=>{ }} >
-                    <View style={{width:'100%',height:60,flexDirection:'row',alignItems:'center'}}>
-                        <Image style={{marginLeft:15,width:22,height:22}} source={require('../../../res/images/iconmyyuyue.png')}></Image>
-                        <Text style={{marginLeft:15,fontSize:16,color:'#333'}}>我的预约c   </Text>
+                <View style={{backgroundColor:'#cdcdcd',height:1,width:'100%'}}></View>
+                <TouchableHighlight  underlayColor='#f2f2f2' style={{backgroundColor:'#d6d6d6'}} onPress={()=>{ }} >
+                    <View style={{width:'100%',height:50,flexDirection:'row',alignItems:'center'}}>
+                        <Image style={{marginLeft:15,width:22,height:22}} source={require('../../../res/images/iconjiaolian.png')}></Image>
+                        <Text style={{marginLeft:15,fontSize:16,color:'#333'}}>预约设置</Text>
                     </View>
                 </TouchableHighlight>
+                <View style={{backgroundColor:'#cdcdcd',height:1,width:'100%'}}></View>
+                <TouchableHighlight  underlayColor='#f2f2f2' style={{backgroundColor:'#d6d6d6'}} onPress={()=>{ }} >
+                    <View style={{width:'100%',height:50,flexDirection:'row',alignItems:'center'}}>
+                        <Image style={{marginLeft:15,width:22,height:22}} source={require('../../../res/images/iconjiaolian.png')}></Image>
+                        <Text style={{marginLeft:15,fontSize:16,color:'#333'}}>关于我们</Text>
+                    </View>
+                </TouchableHighlight>
+                <View style={{backgroundColor:'#cdcdcd',height:1,width:'100%'}}></View>
+                <TouchableHighlight  underlayColor='#f2f2f2' style={{backgroundColor:'#d6d6d6'}} onPress={()=>{ }} >
+                    <View style={{width:'100%',height:50,flexDirection:'row',alignItems:'center'}}>
+                        <Image style={{marginLeft:15,width:22,height:22}} source={require('../../../res/images/iconjiaolian.png')}></Image>
+                        <Text style={{marginLeft:15,fontSize:16,color:'#333'}}>设        置</Text>
+                    </View>
+                </TouchableHighlight>
+                <View style={{backgroundColor:'#cdcdcd',height:1,width:'100%'}}></View>
             </View>
         );
     }
@@ -73,6 +97,6 @@ export default class SelfCenter extends Component {
 const Styles = StyleSheet.create({
     container: {
         flex:1,
-        backgroundColor: '#cdcdcd',
+        backgroundColor: '#d6d6d6',
     }
 });
